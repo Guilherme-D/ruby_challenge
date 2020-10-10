950e879239bde04c1e430cd95d865b385b07a116
Guilherme-D <g.davidmf@gmail.com>
2020-10-10 20:17:32 -0300

    Download icall when access root path

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -1,6 +1,7 @@
 require_relative '../../cm_challenge/absences.rb'
 class AbsencesController < ApplicationController
   def index
-    @ical = CmChallenge::Absences.new.to_ical
+    @ical_file = CmChallenge::Absences.new.to_ical
+    send_data @ical_file, filename: "absences.ics"
   end
 end
